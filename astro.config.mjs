8cc1b70940a8c06fc4bc99506f3b3f23f52b3552
Ahmed Masoud <ahmedmasoud.business@gmail.com>
2026-07-10 12:38:23 +0300

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://ahmed-masoud-seo.vercel.app",
+  integrations: [sitemap()],
 });
